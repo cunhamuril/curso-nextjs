cb27a587a1e8635f7b35d531c087e57fa7dba56d
Murilo Cunha <murilo.sant@hotmail.com>
2020-12-01 13:09:53 -0300

    feat(styles): configuring styled-components

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;    
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+
+    color: #fff;
+    background: #121214;
+  } 
+`;
