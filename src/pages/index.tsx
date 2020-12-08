5f906ef9ec144c02e39903af71e89e658ec7a862
Murilo Cunha <murilo.sant@hotmail.com>
2020-12-08 15:05:46 -0300

    feat(data): client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+
 import { Title } from "../styles/pages/Home";
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([
+    { id: "", title: "" },
+  ]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/recommended").then((response) => {
+      response.json().then((data) => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
-      <Title>Hello NextJS</Title>
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map((recommendedProduct) => (
+            <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
