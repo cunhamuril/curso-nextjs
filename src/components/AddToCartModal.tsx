c0c6b600ccda0d9c21220747fb6fe6b475308823
Murilo Cunha <murilo.sant@hotmail.com>
2020-12-10 12:05:28 -0300

    feat(prod): lazy load components

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,9 @@
+const AddToCartModal: React.FC = () => {
+  return (
+    <div>
+      <h1>Do you really want to add this product to the cart?</h1>
+    </div>
+  );
+};
+
+export default AddToCartModal;
--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+
+/**
+ * Lazy load de componente
+ */
+const AddToCartModal = dynamic(
+  () => import("../../../components/AddToCartModal"),
+  {
+    loading: () => <p>Loading...</p>,
+    ssr: false, // Não vai ser renderizado pelo lado do servidor, sim pelo browser
+  }
+);
 
 export default function Search() {
   const router = useRouter();
 
-  return <h1>{router.query.slug}</h1>;
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+
+  function handleAddToCart() {
+    setIsAddToCartModalVisible(true);
+  }
+
+  return (
+    <div>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      {isAddToCartModalVisible && <AddToCartModal />}
+    </div>
+  );
 }
